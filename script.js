bf3daf9cc9065535c7ca5496ce3d569f1cae3677
Fin-al <finaltbm@gmail.com>
2026-04-07 15:24:00 -0700

    ADDING LAST THING PLEASE WORK

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById("dateOutput").textContent = "Today is " + formattedMonth
 let val1 = "67";
 let val2 = "134";
 let val3 = "19.87";
-let val4 = "greetings";
+let val4 = "greeting";
 
 let num1 = Number(val1);
 let num2 = Number(val2);
